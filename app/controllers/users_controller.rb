36345bbbd86d1e5e0f641afe41fde0a92a2e5e65
Juan Miguel Alvarez <jm.alvarez@descouvre.com>
2019-04-06 09:05:14 +0700

    Initialize simple rails api

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+    protect_from_forgery with: :null_session
+    skip_before_action :verify_authenticity_token
+    # POST /users
+    def create
+      user = User.create!(user_params)
+      if user.save
+        render jsonapi: user
+      else
+        render jsonapi_errors: user.errors
+      end
+    end
+    private
+    def user_params
+      # whitelist params
+      params.require(:_jsonapi)
+      .require(:data)
+      .require(:attributes)
+      .permit(:name, :email)
+    end
+end
---
+++ app/serializable/serializable_user.rb
@@ -0,0 +1,4 @@
+class SerializableUser < JSONAPI::Serializable::Resource
+    type 'users'
+attributes :name, :email
+end
